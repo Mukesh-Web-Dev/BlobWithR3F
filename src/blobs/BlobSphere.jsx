29b55861170342b876a3d54856f536a654aec473
Mukesh K <76239820+Mukeshmiet@users.noreply.github.com>
2026-05-31 22:31:26 +0530

    Initial Commit

---
+++ src/blobs/BlobSphere.jsx
@@ -0,0 +1,57 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useMemo, useRef } from "react";
+import * as THREE from "three";
+
+import vertexShader from "../shaders/vertexSphereShader.glsl?raw";
+import fragmentShader from "../shaders/fragmentSphereShader.glsl?raw";
+import { DecalGeometry } from "three/examples/jsm/Addons.js";
+
+export default function BlobSphere(){
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+  const hover = useRef(false);
+
+  const uniforms = useMemo(
+    () => ({
+      u_intensity: {
+        value: 0.3,
+      },
+      u_time: {
+        value: 0.0,
+      },
+    }),
+    []
+  );
+
+  useFrame((state) => {
+    const { clock } = state;
+    mesh.current.material.uniforms.u_time.value = 0.4 * clock.getElapsedTime();
+
+    mesh.current.material.uniforms.u_intensity.value = THREE.MathUtils.lerp(
+      mesh.current.material.uniforms.u_intensity.value,
+      hover.current ? 0.45 : 0.2,
+      0.2
+    );
+  });
+
+  return (
+    <mesh
+      ref={mesh}
+      position={[0, 0, 0]}
+      scale={1.5}
+      onPointerOver={() => (hover.current = true)}
+      onPointerOut={() => (hover.current = false)}
+    >
+      <icosahedronGeometry args={[2,20]}  />
+      <shaderMaterial
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+        uniforms={uniforms}
+        wireframe={false}
+        side={THREE.DoubleSide}
+      />
+    </mesh>
+  );
+};
+
